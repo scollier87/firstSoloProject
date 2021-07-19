93252b96a79688a869dd62b46ceacdb3c4dab8ef
Sean Collier <sean.collier928@yahoo.com>
2021-07-19 11:22:24 -0700

    worked on landing page

---
+++ frontend/src/components/LandingPage/index.js
@@ -0,0 +1,14 @@
+import "./LandingPage.css";
+
+function LandingPage() {
+    return (
+        <div className="background">
+            <div className="backgroundLayer2">
+
+            </div>
+        </div>
+    )
+}
+
+
+export default LandingPage;
